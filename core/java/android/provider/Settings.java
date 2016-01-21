63a591c44999294b4edf9a76c58b67444b6ef74b
LorDClockaN <davor@losinj.com>
2016-01-21 20:09:08 +0100

    FWB: Volume dialog transparency (1/2)
    Change-Id: Ib926a1254af2a36f71fdbe1bd54c850e29420ac9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4774,6 +4774,12 @@ public final class Settings {
         public static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
         /**
+         * Transparent volume dialog
+         * @hide
+         */
+        public static final String TRANSPARENT_VOLUME_DIALOG = "transparent_volume_dialog";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
