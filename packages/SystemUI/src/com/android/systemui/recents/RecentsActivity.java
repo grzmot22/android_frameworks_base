6b0e3140369d65a42cf04a75e50762712de71778
Grzmot22 <grzmot22@gmail.com>
2016-02-21 14:20:28 +0100

    Fix duplicate
    Change-Id: If5e0ec715ab451d9d5bef27653b4ae010ec8677e

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -499,14 +499,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     }
 
     @Override
-    protected void onResume() {
-        if (mConfig.searchBarEnabled && mConfig.launchedFromHome) {
-            overridePendingTransition(0, 0);
-        }
-        super.onResume();
-    }
-
-    @Override
     protected void onPause() {
         super.onPause();
         if (mAfterPauseRunnable != null) {
